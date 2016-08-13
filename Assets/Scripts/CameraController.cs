1e8d879ce5da75533ce1dd5812e673638c5d870d
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2016-08-14 03:34:11 +0530

    create walls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,11 +5,11 @@ public class CameraController : MonoBehaviour {
 
 	public GameObject player;
 
-	private Vector3 offset;
+//	private Vector3 offset;
 
-	void start() {
-		offset = transform.position - player.transform.position;
-	}
+//	void start() {
+//		offset = transform.position - player.transform.position;
+//	}
 
 	void LateUpdate() {
 		transform.position = player.transform.position + new Vector3 (0, 10, -10);
